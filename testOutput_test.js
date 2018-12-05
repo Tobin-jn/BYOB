2af21973d7e0158eb88038f6dcc12b4e863c79c1
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-05 11:18:27 -0700

    Seed test database

---
+++ testOutput_test.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    "company_Title": "ALTERYX, INC.",
+    "job_position": "JavaScript Software Engineer",
+    "location": "GREATER DENVER AREA"
+  },
+  {
+    "company_Title": "PEAKSWARE",
+    "job_position": "Senior Front-End Software Engineer",
+    "location": "GREATER BOULDER AREA"
+  },
+  {
+    "company_Title": "SRS ACQUIOM",
+    "job_position": "NetSuite Developer",
+    "location": "GREATER DENVER AREA"
+  },
+  {
+    "company_Title": "SENDGRID",
+    "job_position": "Web Developer",
+    "location": "GREATER DENVER AREA"
+  }
+]
