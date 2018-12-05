36b76162945301ccff79ee3c409a5fcd9eb981d3
marcusp619 <marcusp619@gmail.com>
2018-12-05 13:51:16 -0700

    Add test for fetch jobs

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-app.use( bodyParser.json() );
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'devjobs';
+app.locals.title = 'devjobs_test';
 
-app.get('/api/v1/', (request, response) => {
+app.get('/', (request, response) => {
+  response.status(200).send('everything is ok');
+});
 
+app.get('/api/v1/jobs', (request, response) => {
+  database('jobs')
+    .select()
+    .then(jobs => {
+      console.log(jobs)
+      response.status(200).json(jobs);
+    })
+    .catch(error => console.log({message: `Error fetching jobs: ${error}`}));
 });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -4,8 +4,49 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server.js');
 const expect = chai.expect;
+const should = chai.should();
+const config = require('../knexfile')['test'];
+const database = require('knex')(config);
+
 chai.use(chaiHttp);
 
-describe('Server file', () => {
-  
-})
+describe('API Routes', () => {
+  // Run migrations and seeds for test database
+  before(done => {
+    database.migrate
+      .latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      })
+      .done();
+  });
+
+  beforeEach(done => {
+    database.seed
+      .run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      }).done();
+  });
+
+  it('check to see if everything is setup correctly', done => {
+    chai
+      .request(app)
+      .get('/')
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        done();
+      });
+  });
+  it('should return a 200 status code', done => {
+    chai
+      .request(app)
+      .get('/api/v1/jobs')
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        done();
+      })
+  })
+});
