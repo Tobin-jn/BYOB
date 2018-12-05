0df6f2f061b53e355a6be897eb6b1b4df0cc5bd9
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-05 11:53:31 -0700

    Update test data to seed into test database

--- testOutput_test.js
+++ testOutput_test.js
@@ -2,21 +2,41 @@ module.exports = [
   {
     "company_Title": "ALTERYX, INC.",
     "job_position": "JavaScript Software Engineer",
-    "location": "GREATER DENVER AREA"
+    "location": "GREATER DENVER AREA",
+    "url": "www.alteryx.com",
+    "company_size": 50,
+    "job_openings": 5,
+    "perks": "health insurance, dental insurance",
+    "tech_stack": "React.js, Ruby"
   },
   {
     "company_Title": "PEAKSWARE",
     "job_position": "Senior Front-End Software Engineer",
-    "location": "GREATER BOULDER AREA"
+    "location": "GREATER BOULDER AREA",
+    "url": "www.peaksware.com",
+    "company_size": 40,
+    "job_openings": 2,
+    "perks": "health insurance, dental insurance",
+    "tech_stack": "React.js, Ruby"
   },
   {
     "company_Title": "SRS ACQUIOM",
     "job_position": "NetSuite Developer",
-    "location": "GREATER DENVER AREA"
+    "location": "GREATER DENVER AREA",
+    "url": "www.srsacquiom",
+    "company_size": 60,
+    "job_openings": 1,
+    "perks": "health insurance, dental insurance",
+    "tech_stack": "React.js, Ruby"
   },
   {
     "company_Title": "SENDGRID",
     "job_position": "Web Developer",
-    "location": "GREATER DENVER AREA"
+    "location": "GREATER DENVER AREA",
+    "url": "www.sendgrid",
+    "company_size": 100,
+    "job_openings": 4,
+    "perks": "health insurance, dental insurance",
+    "tech_stack": "React.js, Ruby"
   }
 ]
